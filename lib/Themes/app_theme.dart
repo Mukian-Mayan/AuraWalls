39602b8d91057f0235a67024631bd172b4d7a867
Mukian-Mayan <mukianmayan@gmail.com>
2025-09-18 17:25:39 +0300

    colors

---
+++ lib/Themes/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color color1 = Color(0xFFFCFCFC); 
+  static const Color color2 = Color(0xFFFADDFC); 
+  static const Color color3 = Color(0xFFCD3BE8); 
+  static const Color color4 = Color(0xFFA50DE3);
+  static const Color color5 = Color(0xFF2E0051); 
+}
