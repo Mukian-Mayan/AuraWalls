27b47608db862724c48fa2df2c510462cdef6910
Emma <emmachelimo21@gmail.com>
2025-09-27 13:47:22 +0300

    no chnages made

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Joel\StudioProjects\AuraWalls\aura_walls"
+export "FLUTTER_ROOT=C:\flutter_windows_3.37.0-0.1.pre-beta\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ELITEBOOK\Desktop\CODE\AuraWalls"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
